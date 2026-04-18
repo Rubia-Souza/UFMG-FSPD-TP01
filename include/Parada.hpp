b5ce2623d33b5ae0dd75144701db18a72d65ae5f
Rubia <rubia.alice.m.s@gmail.com>
2026-04-18 19:18:29 -0300

    chore: Adiciona documentação na Parada.hpp

--- include/Parada.hpp
+++ include/Parada.hpp
@@ -5,16 +5,43 @@
 
 class Sala;
 
+/**
+ * @brief Representa uma Parada que deve ser feita no caminho
+ * de uma Entidade, associando uma Sala a um tempo de espera.
+ * 
+ */
 class Parada {
     private:
         Sala* sala = nullptr;
         int tempoEspera = -1;
 
     public:
+        /**
+         * @brief Construtor que cria um novo objeto Parada.
+         * 
+         * @param sala A sala onde a entidade deverar entrar e esperar.
+         * @param tempoEspera O tempo que a entidade deve esperar dentro da sala.
+         */
         Parada(Sala* sala, const int tempoEspera);
+
+        /**
+         * @brief Destrutor que destroi o objeto Parada.
+         * 
+         */
         ~Parada();
 
+        /**
+         * @brief Método que retorna a sala que a entidade deve esperar.
+         * 
+         * @return Sala* A sala que a entidade deve esperar.
+         */
         Sala* getSala();
+
+        /**
+         * @brief Método que retorna o tempo que a entidade deve esperar na sala.
+         * 
+         * @return int O tempo que a entidade deve esperar na sala.
+         */
         int getTempoEspera() const;
 };
 
