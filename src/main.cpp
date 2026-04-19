bd7c92a4e8d2d0ab2500e03b4559f4ca8fed0c1e
Rubia <rubia.alice.m.s@gmail.com>
2026-04-19 15:41:32 -0300

    chore: Adiciona descrição das decisões de implementação na main

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,30 @@
 #include "Parada.hpp"
 
 /**
+ * \mainpage Fundamentos Sistemas Paralelos e Distribuidos - TP01 
+ * 
+ * \section Descisões de Implementação: As duas principais classes do projeto
+ * são Entidade e Sala.
+ * 
+ * - Entidade: É uma thread que irá entrar e sair das salas.
+ * - Sala: Regiões que as entidades irão entrar, sair e passar o tempo nelas.
+ * 
+ * Os métodos entrar() e sair() da Sala são responsáveis por controlar a sincronização,
+ * a entrada e a saida das entidades.
+ * 
+ * A função runThreadEntidade() realiza a simulação do caminho das entidades.
+ * 
+ * De forma geral, a Sala tem uma contagem da quantidade de entidades que estão dentro
+ * dela e que estão esperando para entrar. Sempre que uma entidade sai da sala com o
+ * método sair(), o contador é atualizado. Quando ele chega em zero e as condições de 
+ * entrada são atendidas, a Sala notifica as entidades em espera que elas podem entrar. 
+ * Por outro lado, o método de entrar() incrementa o contador de entidades presentes
+ * dentro da sala e, quando a terceira thread entra, a entrada na sala é bloqueada
+ * novamente. Durante todas operações dos métodos entrar() e sair(), a região crítica
+ * está envolvida por um mutex.
+ */
+
+/**
  * @brief Função responsável por ler os dados de entrada e inicializar os vetores
  * de salas e entidades, instanciando os respectivos objetos com os valores lidos.
  * 
